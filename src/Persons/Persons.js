94cbbb8e0a661c1a0497ab3a2e7201284a2f690b
Maciej Uberna <maciej.uberna@gmail.com>
2019-12-02 20:07:08 +0100

    Mod: src/App.js minor correction

--- src/Persons/Persons.js
+++ src/Persons/Persons.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Person from './Person/Person';
 
 const Persons = (props) =>  (
-    props.arr.map((person) => {
+    props.arr.map(person => {
         if (person !== undefined) {
             return <Person click={props.click.bind(this,person.id)} modifyName={props.modifyName.bind(this, person.id)} key={person.id} id={person.id} name={person.name} age={person.age}>{person.children}</Person>;
         } else return null;
