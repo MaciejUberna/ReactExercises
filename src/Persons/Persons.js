58a490e7e4d4e0e95d2cee1abf47ce7fd7a07878
Maciej Uberna <maciej.uberna@gmail.com>
2019-12-02 19:05:13 +0100

    Mod: src/Persons/Persons.js swithech value -> persons so it's more miningful.

--- src/Persons/Persons.js
+++ src/Persons/Persons.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Person from './Person/Person';
 
 const Persons = (props) =>  (
-    props.arr.map((value) => {
-        if (value !== undefined) {
-            return <Person click={props.click.bind(this,value.id)} modifyName={props.modifyName.bind(this, value.id)} key={value.id} id={value.id} name={value.name} age={value.age}>{value.children}</Person>;
+    props.arr.map((person) => {
+        if (person !== undefined) {
+            return <Person click={props.click.bind(this,person.id)} modifyName={props.modifyName.bind(this, person.id)} key={person.id} id={person.id} name={person.name} age={person.age}>{person.children}</Person>;
         } else return null;
     })
 );
