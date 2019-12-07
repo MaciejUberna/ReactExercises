cba564742126ad37bf406d64230b7bf2f20db7d1
Maciej Uberna <maciej.uberna@gmail.com>
2019-12-07 22:19:45 +0100

    Mod: src/Persons/Person/Person.js add: src/Persons/Person/Person.module.css rem: src/Persons/Person/Person.css added mediaquery. USE: ":global" to use items in css globally.

--- src/Persons/Person/Person.js
+++ src/Persons/Person/Person.js
@@ -1,42 +1,17 @@
-//www.styled-components.com: npm install --save styled-components got rid of radium
-import styled from 'styled-components';
 import React from 'react';
-//import './Person.css'
-
-const StyledDiv = styled.div`    
-        width: 80%;
-        margin: 3rem auto;
-        border: 1px solid #eee;
-        box-shadow: 0 2 3 #000;
-        padding: 16px;
-        text-align: center;
-
-        @media (min-width: 500px) {
-            width: 250px;
-        }
-
-`;
+import classes from './Person.module.css'
 
 const Person = (props) => {
     let person = [];
-    //radium style
-    // const style = {
-    //     '@media (min-width: 500px)': {
-    //         width: '450px'
-    //     }
-    // }
     person.push(<p key={props.id.concat('1')}> Nazywam się {props.name} i mam {props.age} lat.</p>);
     if ( props.children ) {
         person.push(<p key={props.id.concat('2')}>A moje hobby to: {props.children} </p>);
     }
     person.push(<input key={props.id.concat('3')} type="text" onChange={props.modifyName} value={props.name}/>)
     return (
-        //radium div
-        // <div className="Person" style={style} onClick={props.click}>
-        <StyledDiv onClick={props.click}>
+        <div className={classes.Person} onClick={props.click}>
             {person}
-        </StyledDiv>
-        // </div>
+        </div>
     );
 }
 
